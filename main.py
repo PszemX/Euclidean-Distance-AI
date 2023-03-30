e3be3148713ca89593a453a990c88c73dfbfd32e
Przemek <przemek.szumczyk@gmail.com>
2023-03-30 13:50:35 +0200

    Initial commit.

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Stworzyłem to repozytorium z kilku powodów:
+
+1. Po pierwsze, żebyśmy nauczyli się lub pogłębili wiedzę na temat pracy z gitem
+2. Po drugie, żeby kod był cały czas dostępny w chmurze
+3. Po trzecie, żebyśmy mogli zobaczyć kto co zmienił, na co wpadł itp
+
+Stworzę jeszcze później stronę, gdzie będziemy zadania pisali sobie, co jest do zrobienia itp.
+Wypiszę też takie mniej-więcej zasady jak mamy korzystać z pushowania kodu oraz robienia zadań, żeby praca była najefektywniejsza
+oraz żebyśmy również pogłębili wiedzę i praktykę z korzystania w systemie git + scrum.
+"""
+
+print("Hello World!")
