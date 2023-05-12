eb00585cdc47f01ea2066554543793bb8627f83f
niksuu <108527214+niksuu@users.noreply.github.com>
2023-05-12 23:33:31 +0200

    Add files via upload
    Short example of AI learning adding method

--- main.py
+++ main.py
@@ -1,61 +1,65 @@
-import tensorflow as tf
 import numpy as np
-from generators import Generator
 
 
-generator = Generator(train_percentage = "80%")
-a, b, c, d = generator.generate(size = 100, min_range=0, max_range=100, type="substractionAB")
-print(a)
-print(b)
+# Definiujemy funkcję aktywacji
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-#Ustawiamy ziarno losowości dla powtarzalności wyników
-np.random.seed(0)
 
+# Definiujemy klasę NeuralNetwork, która będzie reprezentować naszą sieć neuronową
 class NeuralNetwork:
-    def __init__(self, input_size, hidden_size, output_size, learning_rate):
+    def __init__(self, input_size, hidden_size, output_size):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
-        self.learning_rate = learning_rate
-        
-        self.weights1 = np.random.randn(self.input_size, self.hidden_size)
-        self.weights2 = np.random.randn(self.hidden_size, self.output_size)
-    
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-    
+
+        # Inicjujemy wagi losowo
+        self.weights1 = np.random.rand(self.input_size, self.hidden_size)
+        self.weights2 = np.random.rand(self.hidden_size, self.output_size)
+
     def forward(self, X):
-        self.hidden = self.sigmoid(np.dot(X, self.weights1))
-        self.output = np.dot(self.hidden, self.weights2)
-        return self.output
-    
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
-    
-    def linear_derivative(self, x):
-        return np.ones_like(x)
-    
-    def backward(self, X, y, output):
+        # Propagujemy sygnał przez warstwę ukrytą
+        self.hidden = sigmoid(np.dot(X, self.weights1))
+
+        # Propagujemy sygnał przez warstwę wyjściową
+        output = sigmoid(np.dot(self.hidden, self.weights2))
+        return output
+
+    def backward(self, X, y, output, learning_rate):
+        # Liczymy błąd
         error = y - output
-        output_delta = error * self.linear_derivative(output)
-        hidden_error = output_delta.dot(self.weights2.T)
-        hidden_delta = hidden_error * self.sigmoid_derivative(self.hidden)
-        self.weights1 += X.T.dot(hidden_delta) * self.learning_rate
-        self.weights2 += self.hidden.T.dot(output_delta) * self.learning_rate
-    
-    def train(self, X, y, epochs):
+
+        # Obliczamy gradienty dla warstwy wyjściowej
+        output_grad = error * (output * (1 - output))
+
+        # Obliczamy gradienty dla warstwy ukrytej
+        hidden_grad = self.hidden * (1 - self.hidden)
+        hidden_grad = np.dot(output_grad, self.weights2.T) * hidden_grad
+
+        # Aktualizujemy wagi
+        self.weights2 += learning_rate * np.dot(self.hidden.T, output_grad)
+        self.weights1 += learning_rate * np.dot(X.T, hidden_grad)
+
+    def train(self, X, y, learning_rate, epochs):
         for i in range(epochs):
             output = self.forward(X)
-            self.backward(X, y, output)
-    
+            self.backward(X, y, output, learning_rate)
+
     def predict(self, X):
         return self.forward(X)
 
 
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y = np.array([[0], [1], [1], [2]])
+# Tworzymy dane treningowe
+X = np.array([[0, 0],[0, 0],[0, 0],[0, 0], [0, 1], [1, 0], [1, 1],[1, 2]])
+y = np.array([[0],[0],[0],[0], [1], [1], [2],[3]])
 
-nn = NeuralNetwork(input_size=2, hidden_size=3, output_size=1, learning_rate=0.1)
-nn.train(a, b, epochs=10000)
+# Tworzymy sieć neuronową z jedną warstwą ukrytą o 3 neuronach
+nn = NeuralNetwork(2, 3, 1)
 
-print(nn.predict(X))  # powinno zwrócić [[0], [1], [1], [2]]
+# Uczymy sieć neuronową na danych treningowych
+nn.train(X, y, 0.1, 10000)
+
+# Testujemy nauczoną sieć neuronową
+print(nn.predict(np.array([4, 5])))  # powinno zwrócić około 1
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
