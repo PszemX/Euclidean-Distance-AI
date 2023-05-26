c63316d5ba407466d9bda888effdf75dcfe5fa8d
Przemek <przemek.szumczyk@gmail.com>
2023-05-27 01:05:15 +0200

    Change the actual best multiple layer code.

--- multiple_layers.py
+++ multiple_layers.py
@@ -13,7 +13,7 @@ output_size = 1
 learning_rate = 0.0001
 epochs = 3000
 batch_size = 32
-clip_threshold = 1.0  # Adjust the threshold as needed
+clip_threshold = 5.0  # Adjust the threshold as needed
 
 
 class ActivationReLU:
@@ -25,6 +25,64 @@ class ActivationReLU:
     def backward(self, dvalues):
         self.dinputs = dvalues.copy()
         self.dinputs[self.input <= 0] = 0
+        return self.dinputs
+
+
+class ActivationLeakyReLU:
+    def __init__(self, alpha=0.01):
+        self.alpha = alpha
+
+    def forward(self, input):
+        self.input = input
+        self.output = np.where(input > 0, input, self.alpha * input)
+        return self.output
+
+    def backward(self, dvalues):
+        self.dinputs = np.where(self.input > 0, dvalues, self.alpha * dvalues)
+        return self.dinputs
+
+
+class ActivationSoftmax:
+    def forward(self, input):
+        self.input = input
+        exp_values = np.exp(input - np.max(input, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+        return self.output
+
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+        for index, (single_output, single_dvalues) in enumerate(
+            zip(self.output, dvalues)
+        ):
+            single_output = single_output.reshape(-1, 1)
+            jacobian_matrix = np.diagflat(single_output) - np.dot(
+                single_output, single_output.T
+            )
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+        return self.dinputs
+
+
+class ActivationSigmoid:
+    def forward(self, input):
+        self.input = input
+        self.output = 1 / (1 + np.exp(-input))
+        return self.output
+
+    def backward(self, dvalues):
+        sigmoid = 1 / (1 + np.exp(-self.input))
+        self.dinputs = dvalues * sigmoid * (1 - sigmoid)
+        return self.dinputs
+
+
+class ActivationTanh:
+    def forward(self, input):
+        self.input = input
+        self.output = np.tanh(input)
+        return self.output
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * (1 - np.tanh(self.input) ** 2)
         return self.dinputs
 
 
