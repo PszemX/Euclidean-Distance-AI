30234c70f737a5fd2ba1d1a5eb2bd8d595ef9f56
Artur Spiewak <artur.spiewak2967@gmail.com>
2023-05-27 19:12:04 +0200

    Add configureTraining and train methods in NeuralNetwork class

--- multiple_layers.py
+++ multiple_layers.py
@@ -1,20 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Generate training data
-num_samples = 1000
-x = np.random.randint(0, 20, size=(num_samples, 2))
-y = np.sum(x, axis=1)
-
-# Initialize neural network
-input_size = 2
-hidden_sizes = [512, 256, 128]
-output_size = 1
-learning_rate = 0.0001
-epochs = 3000
-batch_size = 32
-clip_threshold = 5.0  # Adjust the threshold as needed
-
 
 class ActivationReLU:
     def forward(self, input):
@@ -125,12 +111,18 @@ class NeuralNetwork:
         # Last hidden layer to output layer
         self.layers.append(Layer(hidden_sizes[-1], output_size, ActivationReLU()))
 
+    def configureTraining(self, epochs=1000, learning_rate=0.0001, batch_size=32, clip_threshold=5.0):
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.clip_threshold = clip_threshold
+
     def forward(self, x):
         for layer in self.layers:
             x = layer.forward(x)
         return x
 
-    def backward(self, x, y, learning_rate, clip_threshold=None):
+    def backward(self, x, y, clip_threshold=None):
         dvalues = 2 * (self.layers[-1].output - y) / y.size
         for layer in reversed(self.layers):
             dvalues = layer.backward(dvalues)
@@ -148,62 +140,79 @@ class NeuralNetwork:
                         clip_threshold,
                         out=layer.dbiases,
                     )
-                layer.weights -= learning_rate * layer.dweights
-                layer.biases -= learning_rate * layer.dbiases
+                layer.weights -= self.learning_rate * layer.dweights
+                layer.biases -= self.learning_rate * layer.dbiases
 
+    def train(self, x, y, num_samples):
+        for self.epoch in range(self.epochs):
+            epoch_loss = 0.0
+
+            # Shuffle training data
+            indices = np.random.permutation(num_samples)
+            x_shuffled = x[indices]
+            y_shuffled = y[indices]
+
+            for idx in range(0, num_samples, self.batch_size):
+                batch_x = x_shuffled[idx : idx + self.batch_size]
+                batch_y = y_shuffled[idx : idx + self.batch_size]
+
+                # Forward pass
+                output = model.forward(batch_x)
+
+                # Backward pass with gradient clipping
+                model.backward(
+                    batch_x,
+                    batch_y.reshape(-1, 1),
+                    clip_threshold=self.clip_threshold,
+                )
+
+                # Compute batch loss
+                batch_loss = np.mean((output - batch_y.reshape(-1, 1)) ** 2)
+                epoch_loss += batch_loss
+
+            # Print progress
+            loss = epoch_loss / (num_samples // self.batch_size)
+            losses.append(loss)
+            if self.epoch % 100 == 0:
+                print(f"Epoch: {self.epoch}, Loss: {loss:.8f}")
+
+            # Adjust learning rate (learning rate decay)
+            if (self.epoch + 1) % 200 == 0:
+                self.learning_rate *= 0.1
+
+            # Check for NaN loss
+            if np.isnan(loss):
+                print("Loss became NaN. Terminating training.")
+                break
+
+        # Plot the loss curve
+        plt.plot(losses)
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.title("Training Loss")
+        plt.show()
+
+
+# Generate training data
+num_samples = 1000
+x = np.random.randint(0, 20, size=(num_samples, 2))
+y = np.sum(x, axis=1)
+
+# Initialize neural network
+input_size = 2
+hidden_sizes = [512, 256, 128]
+output_size = 1
+learning_rate = 0.0001
+epochs = 300
+batch_size = 32
+clip_threshold = 5.0  # Adjust the threshold as needed
 
 # Training loop
 model = NeuralNetwork(input_size, hidden_sizes, output_size)
 losses = []
 
-for epoch in range(epochs):
-    epoch_loss = 0.0
-
-    # Shuffle training data
-    indices = np.random.permutation(num_samples)
-    x_shuffled = x[indices]
-    y_shuffled = y[indices]
-
-    for idx in range(0, num_samples, batch_size):
-        batch_x = x_shuffled[idx : idx + batch_size]
-        batch_y = y_shuffled[idx : idx + batch_size]
-
-        # Forward pass
-        output = model.forward(batch_x)
-
-        # Backward pass with gradient clipping
-        model.backward(
-            batch_x,
-            batch_y.reshape(-1, 1),
-            learning_rate,
-            clip_threshold=clip_threshold,
-        )
-
-        # Compute batch loss
-        batch_loss = np.mean((output - batch_y.reshape(-1, 1)) ** 2)
-        epoch_loss += batch_loss
-
-    # Print progress
-    loss = epoch_loss / (num_samples // batch_size)
-    losses.append(loss)
-    if epoch % 100 == 0:
-        print(f"Epoch: {epoch}, Loss: {loss:.8f}")
-
-    # Adjust learning rate (learning rate decay)
-    if (epoch + 1) % 200 == 0:
-        learning_rate *= 0.1
-
-    # Check for NaN loss
-    if np.isnan(loss):
-        print("Loss became NaN. Terminating training.")
-        break
-
-# Plot the loss curve
-plt.plot(losses)
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
-plt.title("Training Loss")
-plt.show()
+model.configureTraining(epochs=epochs, batch_size=batch_size, clip_threshold=clip_threshold, learning_rate=learning_rate)
+model.train(x, y, num_samples)
 
 # Generate test data
 test_data = np.random.randint(0, 20, size=(100, 2))
